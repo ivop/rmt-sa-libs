cc5213f11cd88e9776450cefee1e230213ed858d
Ivo van Poorten <ivopvp@gmail.com>
2021-09-01 18:22:32 +0200

    use stdint.h instead of compiler internal types like __int16

--- real/sa_c6502/sa_c6502.h
+++ real/sa_c6502/sa_c6502.h
@@ -1,15 +1,17 @@
 #ifndef __SA_6502__
 #define __SA_6502__
 
+#include <stdint.h>
+
 #define TRUE	1
 #define FALSE	0
 
-#define UWORD unsigned __int16
-#define UBYTE unsigned char
-#define WORD unsigned __int16
-#define BYTE unsigned char
-#define SBYTE char
-#define SWORD signed __int16
+#define UWORD   uint16_t
+#define UBYTE   uint8_t
+#define WORD    uint16_t
+#define BYTE    uint8_t
+#define SBYTE   char
+#define SWORD   int16_t
 
 void __declspec(dllexport) C6502_Initialise(BYTE* memory );
 int __declspec(dllexport) C6502_JSR(WORD* adr, BYTE* areg, BYTE* xreg, BYTE* yreg, int* maxcycles);

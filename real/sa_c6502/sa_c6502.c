23df1e426323d07423a87d37127121cae3a499d5
Ivo van Poorten <ivopvp@gmail.com>
2021-09-01 18:10:14 +0200

    mention experimental fork in name description

--- real/sa_c6502/sa_c6502.c
+++ real/sa_c6502/sa_c6502.c
@@ -11,7 +11,7 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 
 void __declspec(dllexport) C6502_About(char** name, char** author, char** description )
 {
-	*name =        (char*)"CPU 6502 emulation, 1.2.4 - cross compiled at 2021-08-31";
+	*name =        (char*)"CPU 6502 emulation, 1.2.4 - experimental fork ivop";
 	*author =      (char*)"Atari800 emulator developer team, http://atari800.sourceforge.net";
 	*description = (char*)"Stripped down version for instructions execution only by C.P.U. 2003";
 }

426725f7fb9ed9bd079595fbc4d8635b9841b127
Ivo van Poorten <ivopvp@gmail.com>
2021-09-02 15:59:11 +0200

    reindent

--- real/sa_pokey/pokey.c
+++ real/sa_pokey/pokey.c
@@ -1,3 +1,4 @@
+
 /* Crippled version for the sa_pokey.dll use only */
 #include <sys/types.h>
 #include <time.h>
@@ -6,11 +7,11 @@
 #include "pokeysnd.h"
 
 /* structures to hold the 9 pokey control bytes */
-UBYTE AUDF[4 * MAXPOKEYS];	/* AUDFx (D200, D202, D204, D206) */
-UBYTE AUDC[4 * MAXPOKEYS];	/* AUDCx (D201, D203, D205, D207) */
-UBYTE AUDCTL[MAXPOKEYS];	/* AUDCTL (D208) */
+UBYTE AUDF[4 * MAXPOKEYS];      /* AUDFx (D200, D202, D204, D206) */
+UBYTE AUDC[4 * MAXPOKEYS];      /* AUDCx (D201, D203, D205, D207) */
+UBYTE AUDCTL[MAXPOKEYS];        /* AUDCTL (D208) */
 int DivNIRQ[4], DivNMax[4];
-ULONG Base_mult[MAXPOKEYS];		/* selects either 64Khz or 15Khz clock mult */
+ULONG Base_mult[MAXPOKEYS];     /* selects either 64Khz or 15Khz clock mult */
 
 int stereo_enabled = TRUE;
 
@@ -18,231 +19,223 @@ UBYTE poly9_lookup[511];
 UBYTE poly17_lookup[16385];
 static ULONG random_scanline_counter;
 
-UBYTE Pokey_GetByte(UWORD addr)
-{
-	return 0xff;
+UBYTE Pokey_GetByte(UWORD addr) {
+    return 0xff;
 }
 
 void Update_Counter(int chan_mask);
 
-int POKEY_siocheck(void)
-{
- 	return (AUDF[CHAN3] == 0x28 || AUDF[CHAN3] == 0x08 || AUDF[CHAN3] == 0x0a)
- 		&& AUDF[CHAN4] == 0x00 && (AUDCTL[0] & 0x28) == 0x28;
+int POKEY_siocheck(void) {
+    return (AUDF[CHAN3] == 0x28 || AUDF[CHAN3] == 0x08 || AUDF[CHAN3] == 0x0a)
+        && AUDF[CHAN4] == 0x00 && (AUDCTL[0] & 0x28) == 0x28;
 }
 
-#define SOUND_GAIN 2        // XXX perhaps here's the gain loss?
+#define SOUND_GAIN 2            // XXX perhaps here's the gain loss?
 
-void Pokey_PutByte(UWORD addr, UBYTE byte)
-{
-	addr &= stereo_enabled ? 0x1f : 0x0f;
-	switch (addr) {
-	case _AUDC1:
-		AUDC[CHAN1] = byte;
-		Update_pokey_sound(_AUDC1, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDC2:
-		AUDC[CHAN2] = byte;
-		Update_pokey_sound(_AUDC2, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDC3:
-		AUDC[CHAN3] = byte;
-		Update_pokey_sound(_AUDC3, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDC4:
-		AUDC[CHAN4] = byte;
-		Update_pokey_sound(_AUDC4, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDCTL:
-		AUDCTL[0] = byte;
+void Pokey_PutByte(UWORD addr, UBYTE byte) {
+    addr &= stereo_enabled ? 0x1f : 0x0f;
+    switch (addr) {
+    case _AUDC1:
+        AUDC[CHAN1] = byte;
+        Update_pokey_sound(_AUDC1, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDC2:
+        AUDC[CHAN2] = byte;
+        Update_pokey_sound(_AUDC2, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDC3:
+        AUDC[CHAN3] = byte;
+        Update_pokey_sound(_AUDC3, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDC4:
+        AUDC[CHAN4] = byte;
+        Update_pokey_sound(_AUDC4, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDCTL:
+        AUDCTL[0] = byte;
 
-		/* determine the base multiplier for the 'div by n' calculations */
-		if (byte & CLOCK_15)
-			Base_mult[0] = DIV_15;
-		else
-			Base_mult[0] = DIV_64;
+        /* determine the base multiplier for the 'div by n' calculations */
+        if (byte & CLOCK_15)
+            Base_mult[0] = DIV_15;
+        else
+            Base_mult[0] = DIV_64;
 
-		Update_Counter((1 << CHAN1) | (1 << CHAN2) | (1 << CHAN3) | (1 << CHAN4));
-		Update_pokey_sound(_AUDCTL, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDF1:
-		AUDF[CHAN1] = byte;
-		Update_Counter((AUDCTL[0] & CH1_CH2) ? ((1 << CHAN2) | (1 << CHAN1)) : (1 << CHAN1));
-		Update_pokey_sound(_AUDF1, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDF2:
-		AUDF[CHAN2] = byte;
-		Update_Counter(1 << CHAN2);
-		Update_pokey_sound(_AUDF2, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDF3:
-		AUDF[CHAN3] = byte;
-		Update_Counter((AUDCTL[0] & CH3_CH4) ? ((1 << CHAN4) | (1 << CHAN3)) : (1 << CHAN3));
-		Update_pokey_sound(_AUDF3, byte, 0, SOUND_GAIN);
-		break;
-	case _AUDF4:
-		AUDF[CHAN4] = byte;
-		Update_Counter(1 << CHAN4);
-		Update_pokey_sound(_AUDF4, byte, 0, SOUND_GAIN);
-		break;
+        Update_Counter((1 << CHAN1) | (1 << CHAN2) | (1 << CHAN3) |
+                       (1 << CHAN4));
+        Update_pokey_sound(_AUDCTL, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDF1:
+        AUDF[CHAN1] = byte;
+        Update_Counter((AUDCTL[0] & CH1_CH2) ? ((1 << CHAN2) | (1 << CHAN1))
+                       : (1 << CHAN1));
+        Update_pokey_sound(_AUDF1, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDF2:
+        AUDF[CHAN2] = byte;
+        Update_Counter(1 << CHAN2);
+        Update_pokey_sound(_AUDF2, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDF3:
+        AUDF[CHAN3] = byte;
+        Update_Counter((AUDCTL[0] & CH3_CH4) ? ((1 << CHAN4) | (1 << CHAN3))
+                       : (1 << CHAN3));
+        Update_pokey_sound(_AUDF3, byte, 0, SOUND_GAIN);
+        break;
+    case _AUDF4:
+        AUDF[CHAN4] = byte;
+        Update_Counter(1 << CHAN4);
+        Update_pokey_sound(_AUDF4, byte, 0, SOUND_GAIN);
+        break;
 // STEREO
-	case _AUDC1 + _POKEY2:
-		AUDC[CHAN1 + CHIP2] = byte;
-		Update_pokey_sound(_AUDC1, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDC2 + _POKEY2:
-		AUDC[CHAN2 + CHIP2] = byte;
-		Update_pokey_sound(_AUDC2, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDC3 + _POKEY2:
-		AUDC[CHAN3 + CHIP2] = byte;
-		Update_pokey_sound(_AUDC3, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDC4 + _POKEY2:
-		AUDC[CHAN4 + CHIP2] = byte;
-		Update_pokey_sound(_AUDC4, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDCTL + _POKEY2:
-		AUDCTL[1] = byte;
-		/* determine the base multiplier for the 'div by n' calculations */
-		if (byte & CLOCK_15)
-			Base_mult[1] = DIV_15;
-		else
-			Base_mult[1] = DIV_64;
+    case _AUDC1 + _POKEY2:
+        AUDC[CHAN1 + CHIP2] = byte;
+        Update_pokey_sound(_AUDC1, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDC2 + _POKEY2:
+        AUDC[CHAN2 + CHIP2] = byte;
+        Update_pokey_sound(_AUDC2, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDC3 + _POKEY2:
+        AUDC[CHAN3 + CHIP2] = byte;
+        Update_pokey_sound(_AUDC3, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDC4 + _POKEY2:
+        AUDC[CHAN4 + CHIP2] = byte;
+        Update_pokey_sound(_AUDC4, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDCTL + _POKEY2:
+        AUDCTL[1] = byte;
+        /* determine the base multiplier for the 'div by n' calculations */
+        if (byte & CLOCK_15)
+            Base_mult[1] = DIV_15;
+        else
+            Base_mult[1] = DIV_64;
 
-		Update_pokey_sound(_AUDCTL, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDF1 + _POKEY2:
-		AUDF[CHAN1 + CHIP2] = byte;
-		Update_pokey_sound(_AUDF1, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDF2 + _POKEY2:
-		AUDF[CHAN2 + CHIP2] = byte;
-		Update_pokey_sound(_AUDF2, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDF3 + _POKEY2:
-		AUDF[CHAN3 + CHIP2] = byte;
-		Update_pokey_sound(_AUDF3, byte, 1, SOUND_GAIN);
-		break;
-	case _AUDF4 + _POKEY2:
-		AUDF[CHAN4 + CHIP2] = byte;
-		Update_pokey_sound(_AUDF4, byte, 1, SOUND_GAIN);
-		break;
-	}
+        Update_pokey_sound(_AUDCTL, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDF1 + _POKEY2:
+        AUDF[CHAN1 + CHIP2] = byte;
+        Update_pokey_sound(_AUDF1, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDF2 + _POKEY2:
+        AUDF[CHAN2 + CHIP2] = byte;
+        Update_pokey_sound(_AUDF2, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDF3 + _POKEY2:
+        AUDF[CHAN3 + CHIP2] = byte;
+        Update_pokey_sound(_AUDF3, byte, 1, SOUND_GAIN);
+        break;
+    case _AUDF4 + _POKEY2:
+        AUDF[CHAN4 + CHIP2] = byte;
+        Update_pokey_sound(_AUDF4, byte, 1, SOUND_GAIN);
+        break;
+    }
 }
 
-void Pokey_Initialise(int *argc, char *argv[])
-{
-	int i;
-	int j;
-	ULONG reg;
+void Pokey_Initialise(int *argc, char *argv[]) {
+    int i;
+    int j;
+    ULONG reg;
 
-	for (i = 0; i < (MAXPOKEYS * 4); i++) {
-		AUDC[i] = 0;
-		AUDF[i] = 0;
-	}
+    for (i = 0; i < (MAXPOKEYS * 4); i++) {
+        AUDC[i] = 0;
+        AUDF[i] = 0;
+    }
 
-	for (i = 0; i < MAXPOKEYS; i++) {
-		AUDCTL[i] = 0;
-		Base_mult[i] = DIV_64;
-	}
+    for (i = 0; i < MAXPOKEYS; i++) {
+        AUDCTL[i] = 0;
+        Base_mult[i] = DIV_64;
+    }
 
-	for (i = 0; i < 4; i++)
-		DivNIRQ[i] = DivNMax[i] = 0;
+    for (i = 0; i < 4; i++)
+        DivNIRQ[i] = DivNMax[i] = 0;
 
-	/* initialise poly9_lookup */
-	reg = 0x1ff;
-	for (i = 0; i < 511; i++) {
-		poly9_lookup[i] = (unsigned char)( reg >> 1);		//!
-		reg |= (((reg >> 5) ^ reg) & 1) << 9;
-		reg >>= 1;
-	}
-	/* initialise poly17_lookup */
-	reg = 0x1ffff;
-	for (i = 0; i < 16385; i++) {
-		poly17_lookup[i] = (unsigned char)(reg >> 9 );		//!
-		for (j = 0; j < 8; j++) {
-			reg |= (((reg >> 5) ^ reg) & 1) << 17;
-			reg >>= 1;
-		}
-	}
+    /* initialise poly9_lookup */
+    reg = 0x1ff;
+    for (i = 0; i < 511; i++) {
+        poly9_lookup[i] = (unsigned char)(reg >> 1);    //!
+        reg |= (((reg >> 5) ^ reg) & 1) << 9;
+        reg >>= 1;
+    }
+    /* initialise poly17_lookup */
+    reg = 0x1ffff;
+    for (i = 0; i < 16385; i++) {
+        poly17_lookup[i] = (unsigned char)(reg >> 9);   //!
+        for (j = 0; j < 8; j++) {
+            reg |= (((reg >> 5) ^ reg) & 1) << 17;
+            reg >>= 1;
+        }
+    }
 
-	random_scanline_counter = time(NULL) % POLY17_SIZE;
+    random_scanline_counter = time(NULL) % POLY17_SIZE;
 }
 
-void POKEY_Frame(void)
-{
-	random_scanline_counter %= AUDCTL[0] & POLY9 ? POLY9_SIZE : POLY17_SIZE;
+void POKEY_Frame(void) {
+    random_scanline_counter %= AUDCTL[0] & POLY9 ? POLY9_SIZE : POLY17_SIZE;
 }
 
-/*****************************************************************************/
-/* Module:  Update_Counter()                                                 */
-/* Purpose: To process the latest control values stored in the AUDF, AUDC,   */
-/*          and AUDCTL registers.  It pre-calculates as much information as  */
-/*          possible for better performance.  This routine has been added    */
-/*          here again as I need the precise frequency for the pokey timers  */
-/*          again. The pokey emulation is therefore somewhat sub-optimal     */
-/*          since the actual pokey emulation should grab the frequency values */
-/*          directly from here instead of calculating them again.            */
-/*                                                                           */
-/* Author:  Ron Fries,Thomas Richter                                         */
-/* Date:    March 27, 1998                                                   */
-/*                                                                           */
-/* Inputs:  chan_mask: Channel mask, one bit per channel.                    */
-/*          The channels that need to be updated                             */
-/*                                                                           */
-/* Outputs: Adjusts local globals - no return value                          */
-/*                                                                           */
-/*****************************************************************************/
+// Module:  Update_Counter()
+// Purpose: To process the latest control values stored in the AUDF, AUDC,
+//          and AUDCTL registers.  It pre-calculates as much information as
+//          possible for better performance.  This routine has been added
+//          here again as I need the precise frequency for the pokey timers
+//          again. The pokey emulation is therefore somewhat sub-optimal
+//          since the actual pokey emulation should grab the frequency values
+//          directly from here instead of calculating them again.
+//
+// Author:  Ron Fries,Thomas Richter
+// Date:    March 27, 1998
+//
+// Inputs:  chan_mask: Channel mask, one bit per channel.
+//          The channels that need to be updated
+//
+// Outputs: Adjusts local globals - no return value 
 
-void Update_Counter(int chan_mask)
-{
+void Update_Counter(int chan_mask) {
 
-/************************************************************/
-/* As defined in the manual, the exact Div_n_cnt values are */
-/* different depending on the frequency and resolution:     */
-/*    64 kHz or 15 kHz - AUDF + 1                           */
-/*    1 MHz, 8-bit -     AUDF + 4                           */
-/*    1 MHz, 16-bit -    AUDF[CHAN1]+256*AUDF[CHAN2] + 7    */
-/************************************************************/
+// As defined in the manual, the exact Div_n_cnt values are
+// different depending on the frequency and resolution:
+//    64 kHz or 15 kHz - AUDF + 1
+//    1 MHz, 8-bit -     AUDF + 4
+//    1 MHz, 16-bit -    AUDF[CHAN1]+256*AUDF[CHAN2] + 7
 
-	/* only reset the channels that have changed */
+    /* only reset the channels that have changed */
 
-	if (chan_mask & (1 << CHAN1)) {
-		/* process channel 1 frequency */
-		if (AUDCTL[0] & CH1_179)
-			DivNMax[CHAN1] = AUDF[CHAN1] + 4;
-		else
-			DivNMax[CHAN1] = (AUDF[CHAN1] + 1) * Base_mult[0];
-		if (DivNMax[CHAN1] < LINE_C)
-			DivNMax[CHAN1] = LINE_C;
-	}
+    if (chan_mask & (1 << CHAN1)) {
+        /* process channel 1 frequency */
+        if (AUDCTL[0] & CH1_179)
+            DivNMax[CHAN1] = AUDF[CHAN1] + 4;
+        else
+            DivNMax[CHAN1] = (AUDF[CHAN1] + 1) * Base_mult[0];
+        if (DivNMax[CHAN1] < LINE_C)
+            DivNMax[CHAN1] = LINE_C;
+    }
 
-	if (chan_mask & (1 << CHAN2)) {
-		/* process channel 2 frequency */
-		if (AUDCTL[0] & CH1_CH2) {
-			if (AUDCTL[0] & CH1_179)
-				DivNMax[CHAN2] = AUDF[CHAN2] * 256 + AUDF[CHAN1] + 7;
-			else
-				DivNMax[CHAN2] = (AUDF[CHAN2] * 256 + AUDF[CHAN1] + 1) * Base_mult[0];
-		}
-		else
-			DivNMax[CHAN2] = (AUDF[CHAN2] + 1) * Base_mult[0];
-		if (DivNMax[CHAN2] < LINE_C)
-			DivNMax[CHAN2] = LINE_C;
-	}
+    if (chan_mask & (1 << CHAN2)) {
+        /* process channel 2 frequency */
+        if (AUDCTL[0] & CH1_CH2) {
+            if (AUDCTL[0] & CH1_179)
+                DivNMax[CHAN2] = AUDF[CHAN2] * 256 + AUDF[CHAN1] + 7;
+            else
+                DivNMax[CHAN2] =
+                    (AUDF[CHAN2] * 256 + AUDF[CHAN1] + 1) * Base_mult[0];
+        } else
+            DivNMax[CHAN2] = (AUDF[CHAN2] + 1) * Base_mult[0];
+        if (DivNMax[CHAN2] < LINE_C)
+            DivNMax[CHAN2] = LINE_C;
+    }
 
-	if (chan_mask & (1 << CHAN4)) {
-		/* process channel 4 frequency */
-		if (AUDCTL[0] & CH3_CH4) {
-			if (AUDCTL[0] & CH3_179)
-				DivNMax[CHAN4] = AUDF[CHAN4] * 256 + AUDF[CHAN3] + 7;
-			else
-				DivNMax[CHAN4] = (AUDF[CHAN4] * 256 + AUDF[CHAN3] + 1) * Base_mult[0];
-		}
-		else
-			DivNMax[CHAN4] = (AUDF[CHAN4] + 1) * Base_mult[0];
-		if (DivNMax[CHAN4] < LINE_C)
-			DivNMax[CHAN4] = LINE_C;
-	}
+    if (chan_mask & (1 << CHAN4)) {
+        /* process channel 4 frequency */
+        if (AUDCTL[0] & CH3_CH4) {
+            if (AUDCTL[0] & CH3_179)
+                DivNMax[CHAN4] = AUDF[CHAN4] * 256 + AUDF[CHAN3] + 7;
+            else
+                DivNMax[CHAN4] =
+                    (AUDF[CHAN4] * 256 + AUDF[CHAN3] + 1) * Base_mult[0];
+        } else
+            DivNMax[CHAN4] = (AUDF[CHAN4] + 1) * Base_mult[0];
+        if (DivNMax[CHAN4] < LINE_C)
+            DivNMax[CHAN4] = LINE_C;
+    }
 }

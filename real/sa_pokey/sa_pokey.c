65020becbcd518a31776df591254c6c30cba9672
Ivo van Poorten <ivopvp@gmail.com>
2021-09-02 02:16:36 +0200

    mention experimemtal fork in about message

--- real/sa_pokey/sa_pokey.c
+++ real/sa_pokey/sa_pokey.c
@@ -18,7 +18,7 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 
 void __declspec(dllexport) Pokey_About(char** name, char** author, char** description )
 {
-	*name =        (char*)"POKEY Chip Simulator, V2.3 (A800 1.2.4)";
+	*name =        (char*)"POKEY Chip Simulator, V2.3 (A800 1.2.4) -- experimental ivop fork";
 	*author =      (char*)"Ron Fries, Atari800 emulator developer team, http://atari800.sourceforge.net";
 	*description = (char*)"Stripped down version for Pokey sound generation only by C.P.U. 2003";
 }

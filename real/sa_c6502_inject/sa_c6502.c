200f5c349cd65df1802fbde517baaece494c803b
Ivo van Poorten <ivopvp@gmail.com>
2021-09-05 19:29:20 +0200

    update about information

--- real/sa_c6502_inject/sa_c6502.c
+++ real/sa_c6502_inject/sa_c6502.c
@@ -16,7 +16,7 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 
 void __declspec(dllexport) C6502_About(char** name, char** author, char** description )
 {
-    *name =        (char*)"CPU 6502 emulation, 1.2.4 - experimental fork ivop";
-    *author =      (char*)"Atari800 emulator developer team, http://atari800.sourceforge.net";
-    *description = (char*)"Stripped down version for instructions execution only by C.P.U. 2003";
+    *name =        (char*)"CPU 6502 emulation, 2021.09";
+    *author =      (char*)"Atari800 emulator developer team, https://atari800.github.io/";
+    *description = (char*)"Stripped down C.P.U. 2003, cleaned up and code injection by ivop 2021";
 }

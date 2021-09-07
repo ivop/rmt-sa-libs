e83624598cd765ddc6168370647007c45a9bbf8f
Ivo van Poorten <ivopvp@gmail.com>
2021-09-07 17:14:45 +0200

    preliminary framework to run libatari800 for pokey emulation. all linking should be fine now. copy libgcc_s_sjlj-1.dll to your Rmt.exe directory, too.

---
+++ real/sa_pokey_libatari800/sa_pokey.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int libatari800_init (int argc, char ** argv);
+
+void __declspec(dllexport) Pokey_Initialise(int *argc, char *argv[]) {
+    fprintf(stderr, "%s: argc=%i\n", __func__, argc);
+    libatari800_init(0, NULL);
+}
+
+void __declspec(dllexport) Pokey_SoundInit(uint32_t freq17,
+                                           uint16_t playback_freq,
+                                           uint8_t num_pokeys) {
+    fprintf(stderr, "%s: freq17=%u playback_freq=%u num_pokeys=%u\n",
+            __func__, freq17, playback_freq, num_pokeys);
+}
+
+void __declspec(dllexport) Pokey_Process(uint8_t *sndbuffer,
+                                         const uint16_t sndn) {
+//    fprintf(stderr, "%s: sndn=%u\n", __func__, sndn);
+}
+
+uint8_t __declspec(dllexport) Pokey_GetByte(uint16_t addr) {
+    fprintf(stderr, "%s: addr=%u\n", __func__, addr);
+    return 0;
+}
+
+void __declspec(dllexport) Pokey_PutByte(uint16_t addr, uint8_t byte) {
+//    fprintf(stderr, "%s: addr=%u byte=%u\n", __func__, addr, byte);
+}
+
+void __declspec(dllexport) Pokey_About(char** name,
+                                       char** author,
+                                       char** description ) {
+    *name        = (char*) "Bleh bleh bleh";
+    *author      = (char*) "Dracula";
+    *description = (char*) "I never say that!";
+}
+

df61c18cc2e19e99aa7e631d4e41d11bc7c360d3
Ivo van Poorten <ivopvp@gmail.com>
2021-09-08 01:49:57 +0200

    also copy a second pokey at $8200+$10 and set its SKCTL to $03

--- real/sa_pokey_libatari800/mirror.s
+++ real/sa_pokey_libatari800/mirror.s
@@ -6,17 +6,26 @@ main
 loop
     lda shadow,x
     sta $d200,x
+    lda shadow2,x
+    sta $d210,x
     dex
     bpl loop
 
     lda shadow+0x0f
+    sta $d20f
+    lda shadow2+0x0f
     sta $d20f
 
     jmp main
 
 
     org $8200
+
 shadow
+:15 dta 0
+    dta 3       ; SKCTL
+
+shadow2
 :15 dta 0
     dta 3       ; SKCTL
 
